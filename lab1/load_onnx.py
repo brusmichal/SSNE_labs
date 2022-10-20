0728c350769c327137c12909167ac06e3a43f1b5
brusmichal <brus.michal@outlook.com>
2022-10-20 02:45:06 +0200

    torch deleted

--- lab1/load_onnx.py
+++ lab1/load_onnx.py
@@ -1,11 +1,11 @@
 import onnx
 import onnxruntime as ort
-import numpy
+import numpy as np
 
 onnx_model = onnx.load("wine_model.onnx")
 onnx.checker.check_model(onnx_model)
 
-onnx_input = torch.tensor([7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.0010, 3.00, 0.45, 8.8])
+onnx_input = np.array([7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.0010, 3.00, 0.45, 8.8]).astype(np.float32)
 actual = 6.
 ort_sess = ort.InferenceSession("wine_model.onnx")
 output = ort_sess.run(None, {'input': onnx_input})
