e90e61a22c8503f591e24d8075c506de5917cca2
brusmichal <brus.michal@outlook.com>
2022-10-21 03:50:33 +0200

    complete redesign

--- lab1/load_onnx.py
+++ lab1/load_onnx.py
@@ -1,11 +1,18 @@
 import onnx
 import onnxruntime as ort
 import numpy as np
+import pandas as pd
 
 onnx_model = onnx.load("wine_model.onnx")
 onnx.checker.check_model(onnx_model)
 
-onnx_input = np.array([7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.0010, 3.00, 0.45, 8.8]).astype(np.float32)
+df = pd.read_csv("valid.csv")
+x_val = df.values[:, :-1]
+y_val = df.values[:, -1]
+print(x_val.shape, y_val.shape)
+
+onnx_input = x_val.sample(n=10).astype(np.float32)
+
 actual = 6.
 ort_sess = ort.InferenceSession("wine_model.onnx")
 output = ort_sess.run(None, {'input': onnx_input})
